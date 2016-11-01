d9c372b8f4ac5ad00cf6164557b835193198bb8c
danielsantamariarod <danielsantamariarod@uniandes.edu.co>
2016-11-01 09:34:25 -0500

    fake key

--- TrelloTaskUploader/src/main/java/greentf/trello/uploaders/TrelloBoardUploader.java
+++ TrelloTaskUploader/src/main/java/greentf/trello/uploaders/TrelloBoardUploader.java
@@ -29,7 +29,7 @@ public class TrelloBoardUploader
 		try 
 		{
 			boardsToAdd = CSVBoardsLoader.load();
-			Trello trelloApi = new TrelloImpl("f0232b37cba133351f64c578935cfffb", "bd4a9249044a0ea176b8315fdd6edc95bde3b191edf60547945aa84034571849");
+			Trello trelloApi = new TrelloImpl("f0232b37cba133351f64c578935cfff", "bd4a9249044a0ea176b8315fdd6edc95bde3b191edf60547945aa8403457184");
 			TrelloBoardUploader boardUploader =new TrelloBoardUploader(boardsToAdd, trelloApi);
 			boardUploader.execute();
 		} 
--- TrelloTaskUploader/src/main/java/greentf/trello/uploaders/TrelloTaskUploader.java
+++ TrelloTaskUploader/src/main/java/greentf/trello/uploaders/TrelloTaskUploader.java
@@ -27,7 +27,7 @@ public class TrelloTaskUploader
 		try 
 		{
 			cardsToAdd = CSVCardsLoader.load();
-			Trello trelloApi = new TrelloImpl("f0232b37cba133351f64c578935cfffc", "bd4a9249044a0ea176b8315fdd6edc95bde3b191edf60547945aa84034571840");
+			Trello trelloApi = new TrelloImpl("f0232b37cba133351f64c578935cfff", "bd4a9249044a0ea176b8315fdd6edc95bde3b191edf60547945aa8403457184");
 			TrelloTaskUploader taskUploader =new TrelloTaskUploader(cardsToAdd, trelloApi);
 			taskUploader.execute();
 			System.out.println("Finished");
